c30bc6f360194bb7fd1ca157906d6d2dde1054d8
suptalentdev <suptalentdev@gmail.com>
2015-08-21 17:51:45 +0900

    Comments is added on lib.rs

--- src/filesystem.rs
+++
@@ -1,6 +0,0 @@
-#[derive(RustcEncodable, RustcDecodable)]
-#[allow(non_snake_case)]
-pub struct FilesystemChange {
-    pub Path: String,
-    pub Kind: u8
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 //! Docker
 #![doc(html_root_url="https://ghmlee.github.io/rust-docker/doc")]
+
+// import external libliries
 extern crate openssl;
 extern crate unix_socket;
 extern crate rustc_serialize;
 
+// declare modules
 mod tcp;
 mod unix;
 mod http;
@@ -14,6 +17,6 @@ pub mod stats;
 pub mod system;
 pub mod image;
 pub mod process;
-pub mod filesystem;
 
+// publicly re-export
 pub use docker::Docker;
